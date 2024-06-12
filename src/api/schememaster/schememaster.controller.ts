3676c47066db61dff0f02d1bfd01e6bd008542fd
Animesh Chakraborty <cy.animesh@gmail.com>
2024-06-12 22:26:29 +0530

    update

--- src/api/schememaster/schememaster.controller.ts
+++ src/api/schememaster/schememaster.controller.ts
@@ -6,21 +6,30 @@ import { UpdateMasterSchemeDTO } from './dto/updateschem.dto';
 
 
 @ApiTags('schememaster')
-@ApiHeader({
-    name: 'token',
-  })
+
 @Controller('api/schememaster')
 export class SchememasterController {
     constructor(private readonly masterSchemeService: SchememasterService) {}
 
+    @ApiHeader({
+      name: 'token',
+    })
     @Post('createschememaster')
     async create(@Body() createMasterSchemeDto: MasterSchemeDTO) {
         return this.masterSchemeService.create(createMasterSchemeDto);
     }
+
+    @ApiHeader({
+      name: 'token',
+    })
     @Get('schemeview/:scheme_sl')
     async getSchemeById(@Param('scheme_sl') scheme_sl: number) {
       return this.masterSchemeService.getSchemeById(scheme_sl);
     }
+
+    @ApiHeader({
+      name: 'token',
+    })
     @Post('updateschme/:scheme_sl')
     async updateMasterScheme(
       @Param('scheme_sl') scheme_sl: number,
@@ -30,6 +39,9 @@ export class SchememasterController {
       return this.masterSchemeService.updateMasterScheme(scheme_sl, updateMasterSchemeDto);
     }
   
+    @ApiHeader({
+      name: 'token',
+    })
     @Get('schemelist/:userIndex')
     async getMasterSchemeExpendituresByUserIndex(@Param('userIndex') userIndex: number) {
         try {
@@ -39,7 +51,9 @@ export class SchememasterController {
             return { success: false, message: 'Failed to fetch master scheme expenditures.', error };
         }
     }
-
+    @ApiHeader({
+      name: 'token',
+    })
     @Get('getAllScheme/:userIndex')
     async getAllScheme(@Param('userIndex') userIndex: number){
         try{
@@ -49,6 +63,9 @@ export class SchememasterController {
             return{ success:false,message:'Failed to fetch master scheme expenditrure.',error};
         }
     }
+    @ApiHeader({
+      name: 'token',
+    })
     @Get('getschmeforallocation')
     @ApiQuery({ name: 'gpCode', required: false, type: Number }) 
     async getschmeforallocation(@Query('blockcode') blockcode: number, @Query('gpCode') gpCode?: number) {
@@ -65,27 +82,39 @@ export class SchememasterController {
     return this.masterSchemeService.getCounts();
   }
 
-  
+  @ApiHeader({
+    name: 'token',
+  })
   @Get('getactionplanreport')
   async getactionplanreport() {
     return this.masterSchemeService.getactionplanreport();
   }
 
+  @ApiHeader({
+    name: 'token',
+  })
   @Get('Summary_Report_on_Annual_Action_Plan')
   async Summary_Report_on_Annual_Action_Plan() {
     return this.masterSchemeService.Summary_Report_on_Annual_Action_Plan();
   }
 
+  @ApiHeader({
+    name: 'token',
+  })
   @Get('Summary_Report_on_Schemes')
   async getSummaryReport() {
     return await this.masterSchemeService.Summary_Report_on_Schemes();
   }
-
+  @ApiHeader({
+    name: 'token',
+  })
   @Get('home_dashboard')
   async getSummaryReportHome() {
     return await this.masterSchemeService.getSummaryReportHome();
   }
-
+  @ApiHeader({
+    name: 'token',
+  })
   @Get('funding-department-wise')
   async getFundingDepartmentWiseReport() {
     return await this.masterSchemeService.getFundingDepartmentWiseReport();
